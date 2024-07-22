0bb44f06c063869d07131aabfa37ff544aa319a7
fachrya11 <104048847+fachrya11@users.noreply.github.com>
2024-07-22 20:32:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import pickle
-import request
+import requests
 from datetime import datetime, timedelta
 
 # Load the trained ARIMA model
